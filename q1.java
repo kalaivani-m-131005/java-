fa0875a43733c6d678622d549ea31b27a85ed970
Kalaivani M <kalaimayilsamy13@gmail.com>
2025-11-18 19:53:26 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,13 @@
+/*1.Print each character in newline from given string */
+
+
+
+package String;
+import java.util.Scanner;
+public class q1 {
+    public static void main(String[] args) {
+        String name="   hi  hello   " ;
+       
+    }
+    
+}
